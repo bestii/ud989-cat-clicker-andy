475d2df213bc817b88ae4f60920cf379503c2b62
Bestin <bestinjohn1993@gmail.com>
2017-05-07 12:08:49 +0530

    Andy's code with JS Hint applied

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ function bindButtonToCat(idNumber){
 	$("#button"+idNumber).click(function(){
 		hideAllCats();
 		$("#cat"+idNumber).show();
-	})
+	});
 }
 
 function bindCounterToCat(idNumber){
-	var cat = "#cat"+idNumber
+	var cat = "#cat"+idNumber;
 	$(cat).click(function(){
 		var count = $(cat+" > .counter").text();
 		count = parseInt(count) + 1;
 		$(cat+" > .counter").text(count);
-	})
+	});
 }
 
 for (var i=1; i<=buttons.length; i++){
